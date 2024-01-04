9ca4ae456ff8144dd4d193fb44773c236ed27a4f
Abdelrahman Gad <abdelgad@hotmail.com>
2024-01-04 23:14:18 +0100

    4

--- print_env_variable.py
+++ print_env_variable.py
@@ -2,12 +2,26 @@ import os
 
 
 def main():
-    env_variable = os.environ.get('MY_ENV_VARIABLE')
-
-    if env_variable:
-        print(f"Value of MY_ENV_VARIABLE: {env_variable}")
+    django_debug = int(os.getenv("DJANGO_DEBUG", default=0))
+    django_secret = os.getenv("DJANGO_SECRET_KEY")
+    django_allowed_hosts = os.getenv("DJANGO_ALLOWED_HOSTS").split(" ")
+    # = os.environ.get('MY_ENV_VARIABLE')
+    
+    if django_debug:
+        print(f"Value of django_debug: {django_debug}")
     else:
-        print("MY_ENV_VARIABLE is not set.")
+        print("django_debug is not set.")
+    
+    if django_secret:
+        print(f"Value of django_secret: {django_secret}")
+    else:
+        print("django_secret is not set.")
+    
+    if django_allowed_hosts:
+        print(f"Value of django_allowed_hosts: {django_allowed_hosts}")
+    else:
+        print("django_allowed_hosts is not set.")
+    
     lol = "Going dumb"
     print(lol)
 
