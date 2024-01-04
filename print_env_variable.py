7d35e4c4ec4c5823cd41a24fefe20029aba4463e
Abdelrahman Gad <abdelgad@hotmail.com>
2024-01-04 23:06:01 +0100

    3

--- print_env_variable.py
+++ print_env_variable.py
@@ -3,11 +3,13 @@ import os
 
 def main():
     env_variable = os.environ.get('MY_ENV_VARIABLE')
-    
+
     if env_variable:
         print(f"Value of MY_ENV_VARIABLE: {env_variable}")
     else:
         print("MY_ENV_VARIABLE is not set.")
+    lol = "Going dumb"
+    print(lol)
 
 
 if __name__ == "__main__":
